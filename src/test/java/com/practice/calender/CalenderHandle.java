3d9b07cad1017ed7eba5ac84dbc72264d14e36c6
Pradeep kumar behera <praadeepkumarstudent2011@gmail.com>
2020-02-16 00:51:53 +0530

    Pradeep temp Area Calender Impliment

---
+++ src/test/java/com/practice/calender/CalenderHandle.java
@@ -0,0 +1,48 @@
+package com.practice.calender;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CalenderHandle {
+	WebDriver driver=null;
+	@BeforeMethod
+	public void Setup() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("http://omayo.blogspot.com/p/page6.html");
+		driver.manage().window().maximize();
+	}
+	@Test
+	public void MainExection() throws ParseException {
+		 driver.switchTo().frame("dateFrame");
+		 driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+		 driver.findElement(By.xpath("//input[@id='datepicker']")).click();
+		 String Date="19/05/1993";
+		 SimpleDateFormat sdate=new SimpleDateFormat("dd/MM/yy"); //Transfer to format 
+		 Date DateConvertion=sdate.parse(Date); //create the Date object using factory method & parse 
+		 System.out.println(DateConvertion);
+		 String sDD=new SimpleDateFormat("dd").format(DateConvertion); //using format get the exact value of date
+		 System.out.println(sDD);
+		 String sMon=new SimpleDateFormat("MMMM").format(DateConvertion);//using format get the exact value of Month if u use "MM" then its give index of month
+		 System.out.println(sMon);
+		 String sYear=new SimpleDateFormat("yyyy").format(DateConvertion);//using format get the exact value of year
+		 System.out.println(sYear);
+		 
+	}
+	@AfterMethod
+	public void TearDown() {
+		//driver.quit();
+	}
+
+}
--- src/test/java/com/practice/poiapi/TestExcelData.java
+++ src/test/java/com/practice/poiapi/TestExcelData.java
@@ -1,7 +1,12 @@
 package com.practice.poiapi;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -18,9 +23,27 @@ public class TestExcelData {
 	
 	@Test(dataProvider = "getData")
 	public void Test(HashMap<String, String> map) {
-		
+		 FileInputStream fis=null;
+		 XSSFWorkbook workbook=null;
 	     map.get("USERID");
 	     map.get("PASSWORD");
+	     
+	     File testFile=new File("C:\\Users\\PRADEEP\\eclipse-workspace\\PracticeSelenium\\resouces\\TestData.xlsx");
+	     try {
+			fis=new FileInputStream(testFile);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+	     try {
+			 workbook=new XSSFWorkbook(fis);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	    
+		System.out.println(workbook.getSheet("LoginCredential").getRow(2).getCell(2).getStringCellValue());//.getRow(0).getCell(0).getStringCellValue()
+	     
 	     
 		
 	 }
